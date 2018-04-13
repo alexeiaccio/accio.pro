855f2a4b8af76f552a5ecb41d35720183d94c9a1
Alexei Accio <32056666+alexeiaccio@users.noreply.github.com>
2018-04-13 18:18:01 +0300

    popper test

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,55 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Link from 'gatsby-link'
-import Accio from '../components/Scroll'
+import { Manager, Reference, Popper } from 'react-popper'
 
-const SecondPage = () => (
-  <div>
-    <Accio />
-    <Link to="/">Go back to the homepage</Link>
-  </div>
+const Example = () => (
+  <Manager>
+    <Reference>
+      {({ ref }) => (
+        <button type="button" ref={ref}>
+          Reference
+        </button>
+      )}
+    </Reference>
+    {ReactDOM.createPortal(
+      <Popper
+        placement="auto"
+        modifiers={{ preventOverflow: { enabled: false } }}
+        eventsEnabled={true}
+        positionFixed={false}>
+        {({ placement, ref, style }) => (
+          <div ref={ref} style={style} data-placement={placement}>
+            Popper
+          </div>
+        )}
+      </Popper>,
+      document.querySelector('#poop')
+    )}
+  </Manager>
 )
+
+class SecondPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      moo: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      moo: true
+    })
+  }
+
+  render() {
+    return (
+    <div id='poop' style={{height: '100vh'}}>
+      {this.state.moo && <Example />}
+      <Link to="/">Go back to the homepage</Link>
+    </div>
+  )}
+}
 
 export default SecondPage
