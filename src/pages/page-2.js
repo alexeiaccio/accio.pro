4940032a2e19b3d4e97c83debe84f6a81aedd3da
Alexei Accio <32056666+alexeiaccio@users.noreply.github.com>
2018-04-16 17:26:28 +0300

    popper test work

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,34 +3,80 @@ import ReactDOM from 'react-dom'
 import Link from 'gatsby-link'
 import { Manager, Reference, Popper } from 'react-popper'
 
-const Example = () => (
-  <Manager>
-    <Reference>
-      {({ ref }) => (
-        <button type="button" ref={ref}>
-          Reference
-        </button>
-      )}
-    </Reference>
-    {ReactDOM.createPortal(
-      <Popper
-        placement="auto"
-        modifiers={{ preventOverflow: { enabled: false } }}
-        eventsEnabled={true}
-        positionFixed={false}>
-        {({ placement, ref, style, arrowProps }) => (
-          <div ref={ref} style={style} data-placement={placement}>
-            Popper
-            <div ref={arrowProps.ref} style={arrowProps.style}>
-              <div style={{width:'10px', height: '10px', borderRadius: '50%', background: 'red'}}></div>
-            </div>
-          </div>
+class Example extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hover: false,
+      click: false,
+    }
+  }
+
+  handleMouseEnter = () => {
+    this.setState({ hover: true })
+  }
+
+  handleMouseLeave = () => {
+    this.setState({ hover: false, click: false })
+  }
+
+  handleMouseDown = () => {
+    this.setState({ click: true, hover: false })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <Manager>
+        <Reference>
+          {({ ref }) => (
+            <button type="button" ref={ref}
+              onMouseEnter={this.handleMouseEnter}
+              onMouseLeave={this.handleMouseLeave}
+              onMouseDown={this.handleMouseDown}
+            >
+              Reference
+            </button>
+          )}
+        </Reference>
+        {this.state.hover && ReactDOM.createPortal(
+          <Popper
+            placement="auto"
+            modifiers={{ preventOverflow: { enabled: true } }}
+            eventsEnabled={true}
+            positionFixed={false}>
+            {({ placement, ref, style, arrowProps }) => (
+              <div ref={ref} style={style} data-placement={placement}>
+                Hover
+                <div ref={arrowProps.ref} style={arrowProps.style}>
+                  <div style={{width:'10px', height: '10px', borderRadius: '50%', background: 'red'}}/>
+                </div>
+              </div>
+            )}
+          </Popper>,
+          document.querySelector('#poop')
         )}
-      </Popper>,
-      document.querySelector('#poop')
-    )}
-  </Manager>
-)
+        {this.state.click && ReactDOM.createPortal(
+          <Popper
+            placement="top"
+            modifiers={{ preventOverflow: { enabled: true } }}
+            eventsEnabled={true}
+            positionFixed={false}>
+            {({ placement, ref, style, arrowProps }) => (
+              <div ref={ref} style={style} data-placement={placement}>
+                Down
+                <div ref={arrowProps.ref} style={arrowProps.style}>
+                  <div style={{width:'10px', height: '10px', borderRadius: '50%', background: 'red'}}/>
+                </div>
+              </div>
+            )}
+          </Popper>,
+          document.querySelector('#poop')
+        )}
+      </Manager>
+    )
+  }
+}
 
 class SecondPage extends React.Component {
   constructor(props) {
