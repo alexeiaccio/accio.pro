f9c71c91845dad62f0fc07dd557055281c5a6280
Alexei Accio <32056666+alexeiaccio@users.noreply.github.com>
2018-05-17 17:19:02 +0300

    animated !!

--- src/components/elements/MailLink.js
+++ src/components/elements/MailLink.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import { Motion, spring, presets } from 'react-motion'
 
 import { tickFuctory } from 'Helpers'
+import Definition from './Definition'
 
 const AnimatedLink = styled.a.attrs({
   style: ({ color }) => ({
@@ -13,20 +14,23 @@ const AnimatedLink = styled.a.attrs({
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
   justify-content: center;
   align-items: center;
   font-family: sans-serif;
-  font-size: 3rem;
+  font-size: var(--base);
   text-decoration: none;
   transition: all .6 ease-in-out;
 `
 
-const LineThrough = styled.span.attrs({
-  style: ({ textDecorationColor }) => ({
-    textDecorationColor
-  })
-})`
-  text-decoration: line-through dashed;
+const Col = styled.div`
+  flex: 1;
+  width: 100%;
+  max-width: 50%;
+  &:first-child {
+    text-align: right;
+  }
 `
 
 class MailLink extends Component {
@@ -35,7 +39,10 @@ class MailLink extends Component {
     this.state = {
       x: 240,
       y: 50,
-      def: 0
+      def: 0,
+      definitions: [
+        this.props.definitions.list[0]
+      ]
     }
   }
 
@@ -54,6 +61,17 @@ class MailLink extends Component {
     tick()
   }
 
+  componentDidUpdate = (prevProps, prevState) => {
+    if(prevState.def !== this.state.def) {
+      let newDefinitions = this.state.definitions.slice()
+      newDefinitions.push(this.props.definitions.list[this.state.def])
+      let cuttedDefinitions = newDefinitions.filter((definition, i) => i > 0 && i < 2)
+      this.setState({
+        definitions: cuttedDefinitions
+      })
+    }
+  }
+
   handleMouseMove = ({pageX, pageY}) => {
     this.setState({
       x: pageX + 100,
@@ -65,40 +83,39 @@ class MailLink extends Component {
     this.handleMouseMove(e.touches[0])
   }
 
-
   render() {
     const { url, definitions } = this.props
     const style = {
       b: spring(this.state.x, presets.gentle),
       g: spring(this.state.y, presets.gentle)
     }
-    const definitionText = definitions.list[this.state.def].text
+
     return (
-      <Motion
-        defaultStyle={{ b: 240, g: 50 }}
-        style={style}
-      >
-      {color =>
-        <AnimatedLink
-          href={url}
-          onMouseMove={this.handleMouseMove}
-          onTouchMove={this.handleTouchMove}
-          onTouchStart={this.handleTouchMove}
-          style={{
-            color: `rgb(0, ${Math.floor(color.g % 255)}, ${Math.floor(color.b % 255)})`
-          }}
+      <Fragment>
+        <Motion
+          defaultStyle={{ b: 240, g: 50 }}
+          style={style}
         >
-        <span>
-        {definitionText.includes('не')
-          ? <LineThrough style={{
-            textDecorationColor: `rgb(${Math.floor(color.b % 255)}, 0, ${Math.floor(color.g % 255)})`
-          }}>{definitionText.replace('не ', '')}</LineThrough>
-          : definitionText
-        }</span>
-        <span> {definitions.sites}</span>
-        </AnimatedLink>
-      }
-      </Motion>
+        {color =>
+          <AnimatedLink
+            href={`${url}?subject=Хочу%20${this.state.definitions[0].text.replace(/\W$/gi, 'й')}%20сайт`}
+            onMouseMove={this.handleMouseMove}
+            onTouchMove={this.handleTouchMove}
+            onTouchStart={this.handleTouchMove}
+            style={{
+              color: `rgb(0, ${Math.floor(color.g % 255)}, ${Math.floor(color.b % 255)})`
+            }}
+          >
+            <Col>
+              <Definition definitions={this.state.definitions} color={color} />
+            </Col>
+            <Col>
+              <span> {definitions.sites}</span>
+            </Col>
+          </AnimatedLink>
+        }
+        </Motion>
+      </Fragment>
     )
   }
 }
--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -1,1 +1,2 @@
 export { tickFuctory } from './tick'
+export { default as s4 } from './s4'
