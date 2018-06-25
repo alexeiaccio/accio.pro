f40f25210fe7060fec10407e51c46b99d297f31f
Alexei Accio <alexei.accio@gmail.com>
2018-06-25 15:48:47 +0300

    fix index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,10 @@
 import React, { Fragment } from 'react'
-import Helmet from 'react-helmet'
 
 import { AccioSVG } from 'Elements'
 
-
-import favicon from './favicon.png'
-
-const IndexPage = ({ data: { homepage: { data } } }) => {
+const IndexPage = () => {
   return (
-    <Fragment>
-      <Helmet
-        title={data.seotitle}
-        meta={[
-          { name: 'description', content: data.seodescription },
-          { name: 'keywords', content: data.seokeywords },
-        ]}
-      >
-        <link rel="icon" type="image/png" sizes="16x16" href={favicon} />
-      </Helmet>
-      <AccioSVG />
-    </Fragment>
+    <AccioSVG />
   )
 }
 
